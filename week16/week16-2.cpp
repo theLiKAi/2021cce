b40a84f215b5b287ca9b5c420b15a11192b944ce
theLiKAi <91170823+theLiKAi@users.noreply.github.com>
2021-12-29 11:56:21 +0800

    Add files via upload

---
+++ week16/week16-2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+     char line[100];
+
+int main()
+{
+   printf("please input a line:");
+   scanf("%s",line);
+   printf("§A­è­è¿é¤J:%s\n",line);
+   }
---
+++ week16/week16-3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+char line[300];
+int main()
+{
+    printf("块3计:-256,0.0000,101010101010111010\n");
+    int n;
+    float f;
+    scanf("%d%f%s",&n,&f,line);
+    printf("计n:%d\n",n);
+    printf("计f:%f\n",f);
+    printf("计line:%s\n",line);
+    }
